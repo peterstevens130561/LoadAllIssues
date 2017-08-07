16244621001463d527bb12f32f9a729dfd5d73ba
Peter Stevens <peter.stevens@bakerhughes.com>
2017-08-07 02:41:52 -0500

    US012 POST api/qualityprofiles/rename Initial commit -created interfaces / command / handler, still empty

---
+++ SonarQube.API/Commands/Implementations/QualityProfiles/Rename/IQualityProfilesRenameCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using PeterSoft.SonarQube.Connector.Client;
+using PeterSoft.SonarQube.Connector.Commands;
+using PeterSoft.SonarQube.Connector.Infrastructure.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterSoft.SonarQube.Connector.CommandHandlers
+{
+    internal class QualityProfilesRenameCommandHandler : ICommandHandler<QualityProfilesRenameCommand>
+    {
+        private readonly RestClient restClient; 
+
+        public QualityProfilesRenameCommandHandler(RestClient restClient)
+        {
+            this.restClient = restClient;
+        }
+
+        public void Execute(QualityProfilesRenameCommand command)
+        {
+            IRestParameters restParameters = command.Parameters();
+            restClient.SetPath(@"qualityprofiles/rename").Post(restParameters);
+
+        }
+    }
+}
---
+++ SonarQube.API/Commands/Implementations/QualityProfiles/Rename/QualityProfilesRenameCommand.cs
@@ -0,0 +1,26 @@
+﻿using PeterSoft.SonarQube.Connector.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PeterSoft.SonarQube.Connector.Models;
+using PeterSoft.SonarQube.Connector.Client;
+using PeterSoft.SonarQube.Connector.Client;
+
+namespace PeterSoft.SonarQube.Connector.Commands
+{
+    class QualityProfilesRenameCommand : ParametersBase, IQualityProfilesRenameCommand
+    {
+        public QualityProfilesRenameCommand(IRestParameters restParameters) : base(restParameters)
+        {
+        }
+
+
+
+
+
+
+
+    }
+}
